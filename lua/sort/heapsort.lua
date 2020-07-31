067374a327a518564758ca3263611e5f3636ce0a
rivarrl <1049793871@qq.com>
2020-07-31 18:32:34 +0800

    2

--- lua/sort/heapsort.lua
+++ lua/sort/heapsort.lua
@@ -1,28 +1,43 @@
 package.path = package.path ..';../utils/?.lua'
+local f = require "common"
+
+-- 交换
+function swap(arr, i, j)
+    local tmp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = tmp
+end
 
 -- 下沉
 function sink(arr, i, n)
     local l = i * 2
     local r = l + 1
     local j = i
-    if l < n and arr[l] > arr[j] then
+    if l <= n and arr[l] > arr[j] then
         j = l
     end
-    if r < n and arr[r] > arr[j] then
+    if r <= n and arr[r] > arr[j] then
         j = r
     end
     if i ~= j then
+        swap(arr, i, j)
         sink(arr, j, n)
     end
 end
 
+-- 堆排序
 function heapsort(arr)
-    for i = #arr, 1 do
-        
+    for i = math.floor(#arr/2), 1, -1 do
+        sink(arr, i, #arr)
+    end
+    swap(arr, 1, #arr)
+    for n = #arr-1, 1, -1 do
+        sink(arr, 1, n)
+        swap(arr, 1, n)
     end
 end
 
-local arr = {4,5,3,1,2}
+local arr = {4,5,7,3,1,6,2}
+f:ptb(arr)
 heapsort(arr)
-local utils = require "common"
-utils.ptb(arr)
+f:ptb(arr)
