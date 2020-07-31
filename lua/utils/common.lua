6e15937f99ea599bd306250f4fcdba4cc6e4efc9
rivarrl <1049793871@qq.com>
2020-07-31 18:33:09 +0800

    2

--- lua/utils/common.lua
+++ lua/utils/common.lua
@@ -2,7 +2,15 @@ local f = {}
 
 -- 打印table
 function f:ptb(tb)
-    print('['..table.concat(tb, ', ')..']')
+    local p
+    for k, v in pairs(tb) do
+        if p == nil then
+            p = v
+        else
+            p = p..', '..v
+        end
+    end
+    print('['..p..']')
 end
 
 -- 翻转数组
